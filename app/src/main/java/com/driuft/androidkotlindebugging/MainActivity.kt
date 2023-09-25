247fa860e82787132e8bdad7bbe71a3445c9c6e2
Mavis Javier <89547162+mavis-javier@users.noreply.github.com>
2023-09-25 10:59:54 -0500

    Uploaded app files

--- app/src/main/java/com/driuft/androidkotlindebugging/MainActivity.kt
+++ app/src/main/java/com/driuft/androidkotlindebugging/MainActivity.kt
@@ -30,7 +30,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun launchHelloWorldActivity() {
-        val i = Intent(404 as Context, HelloWorldActivity::class.java)
+        // Change: '404 as Context' -> 'this'
+        val i = Intent(this, HelloWorldActivity::class.java)
         startActivity(i)
     }
 
